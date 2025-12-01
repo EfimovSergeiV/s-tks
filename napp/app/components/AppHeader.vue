ec44d2f9342edb334b106a118a26da97c443298d
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-12-01 16:57:13 +0300

    add:

--- napp/app/components/AppHeader.vue
+++ napp/app/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-8">
+  <div class="py-8 flex">
     <nuxt-link :to="{ name: 'index' }">
       <img src="/white-logo.png" class="h-10" />
       <!-- <p class="text-gray-300">для сотрудников</p> -->
