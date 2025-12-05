8ecbb4259a19d8ece6f8a465750a092eed4d8d38
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-12-05 11:09:37 +0300

    add:

--- napp/nuxt.config.ts
+++ napp/nuxt.config.ts
@@ -2,10 +2,23 @@ import cfg from "./conf"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-12-05',
   devtools: { enabled: true },
   
   app: {
+    head: {
+      title: 'Внутренние сервисы Техносвар КС',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'format-detection', content: 'telephone=yes' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      ],
+    },
+
+
     pageTransition: { name: 'page', mode: 'out-in' },
   },
 
